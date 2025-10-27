709de7718c504205c21222148c190853e9b17ec8
Uladzislau <u.shylko25@gmail.com>
2025-10-27 21:55:33 +0300

    feat: add hw1

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import s from './App.module.css'
 import HW2 from "../s2-homeworks/hw02/HW2";
 import HW3 from "../s2-homeworks/hw03/HW3";
-/*import HW1 from '../s2-homeworks/hw01/HW1'*/
+import HW1 from '../s2-homeworks/hw01/HW1'
 
 function App() {
     return (
         <div className={s.App}>
-            {/*<HW1/>*/}
+            <HW1/>
             {/*раскомментировать по ходу выполнения*/}
             <HW2/>
             <HW3/>

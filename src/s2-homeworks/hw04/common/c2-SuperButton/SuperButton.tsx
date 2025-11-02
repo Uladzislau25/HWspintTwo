f508a76b92584ef75240bce2d6649192a802ff43
Uladzislau <u.shylko25@gmail.com>
2025-11-02 21:44:44 +0300

    feat: add hw4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,13 +17,13 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+    const finalClassName = `${s.button} ${xType === 'red' ? s.red : xType === 'secondary'? s.secondary : s.default} ${disabled ? s.disabled : ''}`
+        //s.button
+        //+ (xType === 'red' ? ''+ s.red :
+        //    xType === 'secondary' ? ''  + s.secondary : '' + s.default) + (disabled ? '' + s.disabled : '')
 
+                // задачка на смешивание классов
+        //`${s.button} ${xType === 'red' ? s.red : xType === 'secondary'? s.secondary : s.default} ${disabled ? s.disabled : ''}`
     return (
         <button
             disabled={disabled}
